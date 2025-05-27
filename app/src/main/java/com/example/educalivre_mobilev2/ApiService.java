5b2f5f08bc29e9d6e6af48a05dd69cb24c1515ae
beceluiz <luizfernando.z.l.2011@gmail.com>
2025-05-27 16:23:57 -0300

    update: paginação, sorter e tratamento da response
    paginação: aumento de numeros de questões disponives (10 > 182).
    
    sorter: embaralhando um array com todas as provas disponiveis pra sempre trazer uma prova diferente aleatoriamente.
    
    response: tratando response da api em que o "ctx" vem com o link da imagem de "files"

--- app/src/main/java/com/example/educalivre_mobilev2/ApiService.java
+++ app/src/main/java/com/example/educalivre_mobilev2/ApiService.java
@@ -2,9 +2,18 @@ package com.example.educalivre_mobilev2;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiService {
-    @GET("v1/exams/2020/questions")
-    Call<RespostaAPI> listarQuestoes();
+    @GET("v1/exams/{year}/questions")
+    Call<RespostaAPI> listarQuestoes(@Path("year")int year);
+
+    @GET("v1/exams/{year}/questions")
+    Call<RespostaAPI> listarQuestoesPaginadas(
+            @Path("year") int year,
+            @Query("limit") int limit,
+            @Query("offset") int offset
+    );
 }
 
